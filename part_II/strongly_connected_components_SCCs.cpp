ca193fb72b39d28b68f5ae0eb4b3c4d6f59d90e2
André Pinheiro Paes <152342426+deerws@users.noreply.github.com>
2025-10-02 17:39:17 -0300

    Update strongly_connected_components_SCCs.cpp

--- part_II/strongly_connected_components_SCCs.cpp
+++ part_II/strongly_connected_components_SCCs.cpp
@@ -4,24 +4,19 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Verifica se o arquivo foi passado como argumento
     if (argc < 2) {
         cerr << "Uso: " << argv[0] << " <arquivo_grafo>" << endl;
         return 1;
     }
     
     try {
-        // Carrega o grafo do arquivo
         Grafo grafo = Grafo::carregarDoArquivo(argv[1]);
-        
-        // Encontra as componentes fortemente conexas
         vector<vector<int>> componentes = grafo.componentesFortementeConexas();
         
         // Imprime as componentes
         for (size_t i = 0; i < componentes.size(); i++) {
             for (size_t j = 0; j < componentes[i].size(); j++) {
-                // Imprime o índice do vértice (somando 1 para voltar à indexação original)
-                cout << componentes[i][j] + 1;
+                cout << componentes[i][j] + 1; // índice do vértice
                 if (j < componentes[i].size() - 1) {
                     cout << ",";
                 }
