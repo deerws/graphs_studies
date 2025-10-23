2cbebd57e46f144032250df97667020d662e5aa3
André Pinheiro Paes <152342426+deerws@users.noreply.github.com>
2025-10-23 16:17:04 -0300

    Update topological_sort.cpp

--- part_II/topological_sort.cpp
+++ part_II/topological_sort.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
-#include "Grafo.hpp"
+#include <iomanip>
+#include "grafo.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Verifica se o arquivo foi passado como argumento
     if (argc < 2) {
         cerr << "Uso: " << argv[0] << " <arquivo_grafo>" << endl;
         return 1;
     }
     
-    try {
-        // Carrega o grafo do arquivo
-        Grafo grafo = Grafo::carregarDoArquivo(argv[1]);
-        
-        // Executa a ordenação topológica
-        vector<int> ordem = grafo.ordenacaoTopologica();
-        
-        // Imprime a ordem topológica usando os rótulos dos vértices
-        for (size_t i = 0; i < ordem.size(); i++) {
-            cout << grafo.getRotulo(ordem[i]);
-            if (i < ordem.size() - 1) {
-                cout << " , ";
-            }
+    string nomeArquivo = argv[1];
+    
+    Grafo g = Grafo::lerArquivo(nomeArquivo);
+    
+    // Executar algoritmo de Kruskal
+    pair<double, vector<pair<int, int>>> resultado = g.kruskal();
+    double pesoTotal = resultado.first;
+    vector<pair<int, int>> mst = resultado.second;
+    
+    cout << fixed << setprecision(1) << pesoTotal << endl;
+    
+    for (size_t i = 0; i < mst.size(); i++) {
+        cout << (mst[i].first + 1) << "-" << (mst[i].second + 1);
+        if (i < mst.size() - 1) {
+            cout << ", ";
         }
-        cout << endl;
-        
-    } catch (const exception& e) {
-        cerr << "Erro: " << e.what() << endl;
-        return 1;
     }
+    cout << endl;
     
     return 0;
 }
